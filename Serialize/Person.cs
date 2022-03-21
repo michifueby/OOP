05520181fae8af0517f1469e0f9ded072cc1a8fd
michaelfuby <michi.fueby@gmail.com>
2022-03-21 19:56:26 +0100

    Upload Serialize

---
+++ Serialize/Person.cs
@@ -0,0 +1,55 @@
+﻿namespace Serialize
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class Person
+    {
+        [NonSerialized]
+        private string secret;
+
+        public string Firstname
+        {
+
+            get;
+            set;
+        }
+
+        public string Lastname
+        {
+            get;
+            set;
+        }
+
+        public DateTime Birthay
+        {
+            get;
+            set;
+        }
+
+        [OnSerializing]
+        public void OnSerializing(StreamingContext context)
+        {
+            Console.WriteLine("This is getting serialized now!");
+        }
+
+        [OnSerialized]
+        public void OnSerialized(StreamingContext context)
+        {
+            Console.WriteLine("This was serialized now!");
+        }
+
+        [OnDeserializing]
+        public void OnDeserializing(StreamingContext context)
+        {
+            Console.WriteLine("This is getting deserialized now!");
+        }
+
+        [OnDeserialized]
+        public void OnDeserialized(StreamingContext context)
+        {
+            Console.WriteLine("This was deserialized now!");
+        }
+    }
+}
---
+++ Serialize/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Serialize
+{
+    using System;
+    using System.IO;
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Formatters.Binary;
+    using System.Xml.Serialization;
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person firstPerson = new Person() { Firstname = "Michael", Lastname = "Mustermann", Birthay = new DateTime(2000, 3, 1, 7, 0, 0) };
+
+            Person secondPerson = new Person() { Firstname = "Max", Lastname = "Mustermann", Birthay = new DateTime(2008, 10, 8, 7, 0, 0) };
+
+            // Serialize in .txt with binary formatter
+            using (FileStream fileStream = new FileStream("Person.txt", FileMode.Create, FileAccess.ReadWrite))
+            {
+                IFormatter formatter = new BinaryFormatter();
+                
+                formatter.Serialize(fileStream, firstPerson);
+                fileStream.Seek(0, SeekOrigin.Begin);
+
+                var deserializeFirstPerson = (Person)formatter.Deserialize(fileStream);
+            }
+
+            // Serialize in .xml with xmlSerializer
+            using (FileStream xmlStream = new FileStream("Person.xml", FileMode.Create, FileAccess.ReadWrite))
+            {
+                XmlSerializer xmlFormatter = new XmlSerializer(typeof(Person));
+
+                xmlFormatter.Serialize(xmlStream, secondPerson);
+                xmlStream.Seek(0, SeekOrigin.Begin);
+
+                var deserializeSecondPerson = (Person)xmlFormatter.Deserialize(xmlStream);
+            }
+        }
+    }
+}
